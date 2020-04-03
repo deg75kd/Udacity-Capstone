7add89201afab19b5886432eb32b064de88cff8f
deg75kd <kevin.dejesus@gmail.com>
2020-04-02 22:29:36 -0500

    Update 4/2

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,9 @@
 from myapp import app
+import pandas as pd
 import json, plotly
 from flask import render_template
 from scripts.graphs import return_figures
+from scripts.search_song import search_song
 
 @app.route('/')
 @app.route('/index')
@@ -10,6 +12,9 @@ def jumbotron():
 
 @app.route('/eda')
 def eda():
+    """
+
+    """
     figures = return_figures()
 
     # plot ids for the html id tag
@@ -21,3 +26,21 @@ def eda():
     return render_template('eda.html',
                            ids=ids,
                            figuresJSON=figuresJSON)
+
+@app.route('/search')
+def search_page():
+    return render_template('search.html')
+
+@app.route('/go')
+def go():
+    """Processes search item from user
+
+    """
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # get results
+    results_df = search_song(query)
+
+    # This will render the go.html
+    return render_template('go.html',tables=[results_df.to_html])
---
+++ scripts/search_song.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import requests
+import xml.etree.ElementTree as ET
+import urllib
+
+from urllib.request import urlopen
+from sklearn.externals import joblib
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+def search_song(song_name):
+    """Use ChartLyrics API to find song matches
+
+    Args:
+        song_name (string): song name to search for
+
+    Returns:
+        song_df (dataframe): details of matching songs
+    """
+    base_url = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricText?'
+    qs_args = {'lyricText': song_name}
+    search_url = base_url + urllib.parse.urlencode(qs_args)
+
+    response = urllib.request.urlopen(search_url)
+    content = response.read().decode('ascii')
+    data = ET.fromstring(content)
+
+    song_df = pd.DataFrame(columns = ['lyric_id', 'lyric_check_sum', 'artist', 'song'])
+
+    for item in data.iterfind('*'):
+        artist = item.findtext('{http://api.chartlyrics.com/}Artist')
+        song = item.findtext('{http://api.chartlyrics.com/}Song')
+        lyric_id = item.findtext('{http://api.chartlyrics.com/}LyricId')
+        lyric_cs = item.findtext('{http://api.chartlyrics.com/}LyricChecksum')
+    
+        song_df = song_df.append({'lyric_id': lyric_id, 
+                                  'lyric_check_sum': lyric_cs, 
+                                  'artist': artist, 
+                                  'song': song}, 
+                                  ignore_index=True)
+
+    song_df = song_df.dropna(axis=0)
+    result_count = song_df.shape[0]
+
+    # limit results to 10
+    if result_count > 10:
+        song_df = song_df.head(10)
+
+    return song_df

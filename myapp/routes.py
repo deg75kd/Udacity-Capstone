8bd586c5343772f1e4b0077df465a83e34b870b6
deg75kd <kevin.dejesus@gmail.com>
2020-04-03 21:33:06 -0500

    Update 4/3

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,7 @@
 from myapp import app
 import pandas as pd
 import json, plotly
-from flask import render_template
+from flask import render_template, request
 from scripts.graphs import return_figures
 from scripts.search_song import search_song
 
@@ -39,8 +39,16 @@ def go():
     # save user input in query
     query = request.args.get('query', '') 
 
+    """watch for stop words - make sure all words not eliminated
+    about, after, all, also, an, and, another, any, are, as, at, be, because, been, before, being, between, both, but, by, came, can, come, could, did, do, does, each, else, for, from, get, got, had, has, have, he, her, here, him, himself, his, how, if, in, into, is, it, its, just, like, make, many, me, might, more, most, much, must, my, never, no, now, of, on, only, or, other, our, out, over, re, said, same, see, should, since, so, some, still, such, take, than, that, the, their, them, then, there, these, they, this, those, through, to, too, under, up, use, very, want, was, way, we, well, were, what, when, where, which, while, who, will, with, would, you, your
+
+    """
+
     # get results
     results_df = search_song(query)
 
     # This will render the go.html
-    return render_template('go.html',tables=[results_df.to_html])
+    return render_template('go.html', 
+                           query = query, 
+                           table = results_df
+    )

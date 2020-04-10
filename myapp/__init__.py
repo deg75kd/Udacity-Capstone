69e551c408c58aced67c75b5856f0de27b4a0b36
deg75kd <kevin.dejesus@gmail.com>
2020-04-09 21:35:09 -0500

    Update 4/9 #3

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -3,14 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from myapp import routes
-
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+from scripts.myfunctions import tokenize
+# def tokenize(text):
+    # tokens = word_tokenize(text)
+    # lemmatizer = WordNetLemmatizer()
     
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    # clean_tokens = []
+    # for tok in tokens:
+        # clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        # clean_tokens.append(clean_tok)
         
-    return clean_tokens
+    # return clean_tokens
